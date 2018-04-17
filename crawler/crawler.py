3a3c3f268885ba88905ea7bfd9d1844ed322db71
Zesen Wang <451324565@qq.com>
2018-04-17 19:21:46 +0200

    Add files via upload

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -25,14 +25,24 @@ def get_product_info(url):
 	
 	# Get Basic Information about Product
 	product = {
-		"title": soup.select("#titletextonly")[0].contents[0],
+		"title": None,
 		"price": None,
-		"description": soup.select("meta[name=description]")[0]['content'],
+		"description": None,
 		"date": soup.select("time[class='date timeago']")[0]['datetime'],
 		"condition": None,
 		"imgs": [],
 		"url": url
 	}
+	try:
+		product["description"] = soup.select("meta[name=description]")[0]['content']
+	except:
+		product["description"] = ""
+
+	try:
+		product["title"] = soup.select("#titletextonly")[0].contents[0]
+	except:
+		product["title"] = ""
+
 	try:
 		product["condition"] = soup.select("p[class='attrgroup'] span b")[0].contents[0]
 	except:
@@ -59,6 +69,8 @@ regions = ["sfbay", "bakersfield", "chico", "fresno", "goldcountry", "hanford", 
 			"merced", "modesto", "monterey", "redding", "reno", "sacramento", "slo", "santamaria",
 			"stockton", "susanville", "visalia", "yubasutter"]
 
+regions = ["sfbay"]
+
 kinds = ["ata", "ppa", "ara", "sna", "pta", "wta", "ava", "baa", "bar", "haa", "bip", "bia", "bpa", "boo",
 		"bka", "bfa", "cta", "ema", "moa", "cla", "cba", "syp", "sya", "ela", "gra", "zip", "fua", "gms",
 		"foa", "hva", "hsa", "jwa", "maa", "mpa", "mca", "msa", "pha", "rva", "sga", "tia", "tla", "taa",
@@ -75,16 +87,40 @@ headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHT
 
 n_product_page = 120
 
-products = dict((region, []) for region in regions)
+# products = dict((region, []) for region in regions)
+
 
 for region in regions:
 	for kind in kinds:
+		products = []
+		if kinds.index(kind) <= kinds_name.index("bike parts"):
+			continue
+
 		max_result = get_max_result(region, kind)
+		max_result = min(max_result, 3000)
 		print("Region: {}, Kind: {}".format(region, kinds_name[kinds.index(kind)]))
 		with progressbar.ProgressBar(max_value = max_result) as bar:
 			for i in range(max_result // n_product_page):
 				prod_urls = get_all_product_url(region, kind, i * n_product_page)
+				
 				for j in range(n_product_page):
-					products[region].append(get_product_info(prod_urls[j]))
+					products.append(get_product_info(prod_urls[j]))
 					bar.update(j + 1 + i * n_product_page)
+
+			if max_result < n_product_page:
+				prod_urls = get_all_product_url(region, kind, 0)
+				for j in range(max_result):
+					products.append(get_product_info(prod_urls[j]))
+					bar.update(j + 1)
+
+			elif max_result % n_product_page != 0:
+				prod_urls = get_all_product_url(region, kind, max_result - n_product_page)
+				for j in range(max_result % n_product_page):
+					products.append(get_product_info(prod_urls[n_product_page - j - 1]))
+					bar.update((max_result // n_product_page) * n_product_page + j + 1)
+			
+		f = open("output{}.txt".format(kinds.index(kind)), "w")
+		f.write(json.dumps(products))
+		f.close()
+		del products
 		print (region, kind)
